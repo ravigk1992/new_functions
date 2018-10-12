69b5c352c4dcb6a01a10fc935f03e529d4043ce7
ravigk1992 <ravigk1992@gmail.com>
2018-10-12 13:07:44 +0530

    third program

---
+++ Addition1.java
@@ -0,0 +1,12 @@
+package com.morningBatch;
+
+public class Addition {
+
+	public static void main(String[] args) {
+
+		int r=16,s=34,t;
+		t=r+s;
+		System.out.println("addition of r and s is:"+t);
+	}
+
+}
